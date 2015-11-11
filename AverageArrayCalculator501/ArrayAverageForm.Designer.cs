02da285d5d553e8d3e3486a090e60805560e1f78
pirates19 <nicholas.ellul1@gmail.com>
2015-11-11 14:12:30 -0500

    finished

---
+++ AverageArrayCalculator501/ArrayAverageForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace AverageArrayCalculator501
+{
+    partial class ArrayAverageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGO = new System.Windows.Forms.Button();
+            this.lstArrayNumbers = new System.Windows.Forms.ListBox();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGO
+            // 
+            this.btnGO.Location = new System.Drawing.Point(105, 212);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(75, 23);
+            this.btnGO.TabIndex = 0;
+            this.btnGO.Text = "Generate";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // lstArrayNumbers
+            // 
+            this.lstArrayNumbers.FormattingEnabled = true;
+            this.lstArrayNumbers.Location = new System.Drawing.Point(82, 35);
+            this.lstArrayNumbers.Name = "lstArrayNumbers";
+            this.lstArrayNumbers.Size = new System.Drawing.Size(120, 134);
+            this.lstArrayNumbers.TabIndex = 1;
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(96, 177);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(84, 13);
+            this.lblAverage.TabIndex = 2;
+            this.lblAverage.Text = "The average is: ";
+            this.lblAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(284, 24);
+            this.mnuStrip.TabIndex = 3;
+            this.mnuStrip.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // ArrayAverageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lstArrayNumbers);
+            this.Controls.Add(this.btnGO);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "ArrayAverageForm";
+            this.Text = "Average Number";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.ListBox lstArrayNumbers;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
---
+++ AverageArrayCalculator501/ArrayAverageForm.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * Created by: Nicholas  Ellul
+ * Created on: 11-Nov-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 5-01
+ * This program adds nuumbers 1 though 10, then says average of numbers.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AverageArrayCalculator501
+{
+    public partial class ArrayAverageForm : Form
+    {
+        //global declaration
+        int[] arrayOfNumbers = new int[10];
+        Random randomNumbers = new Random();
+
+       
+        public ArrayAverageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+            //input
+            const int NUMBER_OF_ITEMS = 10;
+            int randomizedNumber;
+
+            //process
+            this.lstArrayNumbers.Items.Clear();
+
+            for (int counter = 0; counter < NUMBER_OF_ITEMS; counter++)
+            {
+                randomizedNumber = randomNumbers.Next(1, 10 + 1);
+                this.lstArrayNumbers.Items.Add(randomizedNumber);
+                arrayOfNumbers[counter] = randomizedNumber;
+            }
+
+            //output
+            this.lblAverage.Text = "The average is: " + arrayOfNumbers.Sum()/NUMBER_OF_ITEMS;
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
